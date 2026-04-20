18a809e139b7c9f3c2579e4827b0be8e8494ba55
RenaKhalilova-create <rena.khalilova2006@gmail.com>
2026-04-20 13:34:48 +0400

    Adding find, delete, menu, organizational changes

--- main.cpp
+++ main.cpp
@@ -3,24 +3,74 @@
 #include <fstream>
 using namespace std;
 
-struct Student {
+struct Student{
     string name;
     int age;
 };
 
 vector<Student> students;
 
-void addStudent() {
+void addStudent(){
     Student s;
-    cout << "Enter name: ";
-    cin >> s.name;
-    cout << "Enter age: ";
-    cin >> s.age;
+    cout<< "Enter name: ";
+    cin>> s.name;
+    cout<< "Enter age: ";
+    cin>> s.age;
     students.push_back(s);
 }
 
-void showStudents() {
-    for (const auto& s : students) {
-        cout << s.name << " - " << s.age << endl;
+void showStudents(){
+    for(const auto& s: students){
+        cout<< s.name << " - " << s.age <<endl;
     }
+}
+
+void findStudent(){
+    string name;
+    cout<< "Enter name to search: ";
+    cin>> name;
+
+    for(const auto& s: students){
+        if(s.name == name){
+            cout<< "Found: " << s.name << " - " << s.age <<endl;
+            return;
+        }
+    }
+    cout<< "Student not found\n";
+}
+
+void deleteStudent(){
+    string name;
+    cout<< "Enter name to delete: ";
+    cin>> name;
+
+    for(int i = 0; i < students.size(); i++){
+        if(students[i].name == name){
+            students.erase(students.begin() + i);
+            cout<< "Deleted successfully\n";
+            return;
+        }
+    }
+    cout<< "Student not found\n";
+}
+
+int main(){
+    int choice;
+
+    while(true){
+        cout<< "\n1. Add student\n";
+        cout<< "2. Show students\n";
+        cout<< "3. Find student\n";
+        cout<< "4. Delete student\n";
+        cout<< "5. Exit\n";
+
+        cin>> choice;
+
+        if(choice == 1) addStudent();
+        else if(choice == 2) showStudents();
+        else if(choice == 3) findStudent();
+        else if(choice == 4) deleteStudent();
+        else if(choice == 5) break;
+    }
+    return 0;
 }
